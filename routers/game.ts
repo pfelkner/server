d70b8ea1accf9878c718d3ba7e75a15dfbade8f5
Paul Felkner <paul.felkner@elunic.com>
2024-03-17 22:58:49 +0100

    get gameservice before use

--- routers/game.ts
+++ routers/game.ts
@@ -5,23 +5,24 @@ const router = Router();
 const gs = GameService.getInstance();
 
 router.get("/start", async (req, res) => {
+  const gs = GameService.getInstance();
   const game = gs.startGame();
-
   res.send("Game started");
 });
 
 router.get("/nextRound", async (req, res) => {
+  const gs = GameService.getInstance();
   const data = await gs.game?.getRoundData();
   console.log("nextRound".repeat(20));
   console.log(data);
   console.log(gs);
-  console.log(gs.game);
   console.log("nextRound".repeat(20));
   gs.game?.next();
   res.json(data);
 });
 
 router.post("/gameover", async (req, res) => {
+  const gs = GameService.getInstance();
   const userId = req.body.userId;
   gs.game?.stop(userId);
   // game.stop();
